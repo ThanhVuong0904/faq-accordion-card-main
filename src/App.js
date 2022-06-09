3d5f971eb593df78b332648ac3b5cde46a5fd869
ThanhVuong0904 <boypro0409@gmail.com>
2022-06-09 14:25:42 +0700

    fix warnning rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
             </div>
             <div class="attribution">
                 Challenge by{' '}
-                <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+                <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
                     Frontend Mentor
                 </a>
                 . Coded by <a href="https://www.facebook.com/thanhvgg/">Thanh Vuong</a>.
